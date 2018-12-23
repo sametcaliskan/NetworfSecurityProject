c3349a797d27f68fd24b56fd4d2cd580c030468d
seyma görücü <seyma_grc@hotmail.com>
2018-12-23 18:17:28 +0300

    new

--- NetworkSecurity1/src/main/java/com/security/view/AdminView.java
+++ NetworkSecurity1/src/main/java/com/security/view/AdminView.java
@@ -37,7 +37,7 @@ public class AdminView {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 308);
+		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
